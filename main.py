e6661affe8e047a04ada3904d03d40d82a0ed99c
mdkeydama23 <mdk@keydama.com>
2023-12-12 19:24:51 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('example.html')
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+
+if __name__ == '__main__':
+    app.run()
